a6213fe94b7f9685c8318e63533b4bfe20b40776
Phú Nguyễn <phu.nt2710@gmail.com>
2026-07-28 12:36:19 +0700

    Enhance home side panels styling for large screens

--- src/components/home/HomeClientExperience.tsx
+++ src/components/home/HomeClientExperience.tsx
@@ -465,7 +465,7 @@ export function HomeClientExperience() {
       </div>
 
       {/* LEFT PANEL: Memory Shelf (Primary Actions) */}
-      <div className="relative z-10 mt-8 w-full max-w-[23rem] px-3 sm:max-w-3xl sm:px-4 xl:absolute xl:bottom-6 xl:left-6 xl:top-24 xl:mt-0 xl:w-[24rem] xl:max-w-none xl:px-0 xl:pb-0 2xl:w-[34rem] xl:overflow-y-auto xl:[scrollbar-width:none] xl:[&::-webkit-scrollbar]:hidden">
+      <div className="home-side-panel home-side-panel--shelf relative z-10 mt-8 w-full max-w-[23rem] px-3 sm:max-w-3xl sm:px-4 xl:absolute xl:bottom-6 xl:left-6 xl:top-24 xl:mt-0 xl:w-[24rem] xl:max-w-none xl:px-0 xl:pb-0 2xl:w-[34rem] xl:overflow-y-auto xl:[scrollbar-width:none] xl:[&::-webkit-scrollbar]:hidden">
         <div className="flex flex-col relative z-10 xl:pr-3 xl:pb-12 min-h-full">
           <LivingMemoryShelf className="home-shelf relative min-h-[500px] flex-1">
             <DeskCat motionActive={ambientMotion.isActive} isPhone={ambientMotion.isPhone} />
@@ -477,7 +477,7 @@ export function HomeClientExperience() {
       </div>
 
       {/* RIGHT PANEL: Storytelling & Ambient Cards */}
-      <div className="relative z-10 mt-4 w-full max-w-[23rem] overflow-x-clip px-3 pb-32 sm:max-w-3xl sm:px-4 xl:absolute xl:bottom-6 xl:right-6 xl:top-24 xl:mt-0 xl:w-[24rem] xl:max-w-none xl:px-0 xl:pb-0 2xl:w-[32rem] xl:overflow-y-auto xl:[scrollbar-width:none] xl:[&::-webkit-scrollbar]:hidden">
+      <div className="home-side-panel home-side-panel--desk relative z-10 mt-4 w-full max-w-[23rem] overflow-x-clip px-3 pb-32 sm:max-w-3xl sm:px-4 xl:absolute xl:bottom-6 xl:right-6 xl:top-24 xl:mt-0 xl:w-[24rem] xl:max-w-none xl:px-0 xl:pb-0 2xl:w-[32rem] xl:overflow-y-auto xl:[scrollbar-width:none] xl:[&::-webkit-scrollbar]:hidden">
         <CozyDetails motionActive={ambientMotion.isActive} isPhone={ambientMotion.isPhone} />
         <AmbientManager />
         
